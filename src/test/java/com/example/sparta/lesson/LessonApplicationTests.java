2baa0fa65bd8139b5e50b297dcef9054377416a1
kingjinyong <wlsdydwhswkf@gmail.com>
2026-03-12 21:22:55 +0900

    first commit

---
+++ src/test/java/com/example/sparta/lesson/LessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sparta.lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
