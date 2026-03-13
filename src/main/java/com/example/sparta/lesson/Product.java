02c994f226abe41b63a45d7ae5e8bdf7441721a6
kingjinyong <wlsdydwhswkf@gmail.com>
2026-03-13 17:42:57 +0900

    연관관계 매핑 추가

--- src/main/java/com/example/sparta/lesson/Product.java
+++ src/main/java/com/example/sparta/lesson/Product.java
@@ -2,9 +2,12 @@ package com.example.sparta.lesson;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -23,6 +26,10 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
 
     @Column(nullable = false)
     private String name;
